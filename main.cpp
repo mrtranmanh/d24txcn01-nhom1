1d3a020d5d85b773fe5cad8e02010824a813bc29
Tran Duy Manh <mr.taduma@gmail.com>
2025-03-13 23:32:31 +0700

    Update: thay doi ten truong va thu tu luu (Manh)

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
         cout << "\n1. Dang ky\n2. Dang nhap\n3. Thoat\nChon: ";
         
         if (!(cin >> lua_chon)) { // Kiem tra neu nhap sai (vi du: nhap chu thay vi so)
-            cout << "❌ Loi: Vui long nhap so tu 1 den 3!\n";
+            cout << "Loi: Vui long nhap so tu 1 den 3!\n";
             cin.clear();  // Xoa trang thai loi cua cin
             cin.ignore(1000, '\n'); // Xoa du lieu sai con trong bo dem
             continue; // Quay lai vong lap de nhap lai
@@ -20,7 +20,7 @@ int main() {
             case 1: registerUser(); break;
             case 2: loginUser(); break;
             case 3: cout << "Tam biet!\n"; break;
-            default: cout << "❌ Lua chon khong hop le! Vui long nhap so tu 1 den 3.\n"; break;
+            default: cout << "Lua chon khong hop le! Vui long nhap so tu 1 den 3.\n"; break;
         }
 
     } while (lua_chon != 3);
